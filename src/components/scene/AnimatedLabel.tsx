5b9b145f2eb032e05b9355fc234a06537afdcc54
Sacha Morgese <sachamorgese@gmail.com>
2022-09-28 18:31:59 +0100

    Added darkened background when pressing

--- src/components/scene/AnimatedLabel.tsx
+++ src/components/scene/AnimatedLabel.tsx
@@ -14,12 +14,26 @@ export default function AnimatedLabel({
   setterFn,
   children,
 }: Props) {
+  const [isHeldDown, setIsHeldDown] = React.useState(false);
+
   return (
     <animated.span
-      style={style}
-      onPointerDown={() => setterFn(value)}
-      onPointerLeave={() => setterFn('')}
-      onPointerUp={() => setterFn('')}
+      style={{
+        ...style,
+        background: isHeldDown ? '#00315F' : style.background,
+      }}
+      onPointerDown={() => {
+        setIsHeldDown(true);
+        setterFn(value);
+      }}
+      onPointerLeave={() => {
+        setIsHeldDown(false);
+        setterFn('');
+      }}
+      onPointerUp={() => {
+        setIsHeldDown(false);
+        setterFn('');
+      }}
       onContextMenu={(e) => e.preventDefault()}
     >
       {children}
