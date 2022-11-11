3394e2dbf0c7f4bdd3618caa7e2956fa6390d2dc
cqbent <cqbent@gmail.com>
2022-11-11 13:30:38 -0500

    2nd commit

--- src/components/navigation/Sensory.tsx
+++ src/components/navigation/Sensory.tsx
@@ -127,7 +127,9 @@ function Sensory({ path, prefix }: Props) {
           )}
           {hasValue && (
             <div className='nav-middle-container'>
-              <div className='description'><Trans i18nKey={t(value + '.description')} /></div>
+              <div className='description'>
+                <Trans i18nKey={t(value + '.description')} />
+              </div>
               <button
                 className='continue-button'
                 onClick={() => {
