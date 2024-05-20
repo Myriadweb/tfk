d1e7186a50c4272ac564d954e6ad1ce3630bb404
cqbent <cqbent@gmail.com>
2024-05-20 16:23:01 -0400

    digestivegame update

--- src/components/scene/DigestiveGame.tsx
+++ src/components/scene/DigestiveGame.tsx
@@ -219,7 +219,6 @@ export default function DigestiveGame() {
   const childAsset = STEP_TO_CHILD_CONFIG[step] || 'digestiveEyesClosed';
 
   const ChildComponent = Characters[childAsset][selectedCharacter];
-  console.log('ChildComponent', childAsset)
   const width = sensoryChildWidth;
 
   return (
